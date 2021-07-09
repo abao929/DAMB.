97aeffe100a615572511ffa308b4cefdee5d9e8d
DustInMyGit <dustin.h.wu@gmail.com>
2021-07-09 00:11:34 -0400

    updated train-test-split.py to move things within the data folder

--- train-test-split.py
+++ train-test-split.py
@@ -3,8 +3,8 @@ import os
 import csv
 
 def tt_split(genre):
-    folder_name = genre
-    album_covers = os.listdir(folder_name)
+    cur_folder = os.path.join('data', genre)
+    album_covers = os.listdir(cur_folder)
     random.shuffle(album_covers)
     n_covers = len(album_covers)
     n_test = n_covers // 5 # 80:20 split
@@ -14,21 +14,21 @@ def tt_split(genre):
     # print(len(test_covers))
     # print(len(train_covers))
 
-    os.makedirs(os.path.join('test', folder_name), exist_ok=True)
-    os.makedirs(os.path.join('train', folder_name), exist_ok=True) 
+    os.makedirs(os.path.join('data', 'test', genre), exist_ok=True)
+    os.makedirs(os.path.join('data', 'train', genre), exist_ok=True) 
 
 
     for cover in test_covers:
-        os.rename(os.path.join(folder_name, cover), os.path.join('test', folder_name, cover))
+        os.rename(os.path.join(cur_folder, cover), os.path.join('data', 'test', genre, cover))
     for cover in train_covers:
-        os.rename(os.path.join(folder_name, cover), os.path.join('train', folder_name, cover))
+        os.rename(os.path.join(cur_folder, cover), os.path.join('data', 'train', genre, cover))
 
 def tt_split_csv(genre):
     file_name = genre + '.csv'
     test_rows = []
     train_rows = []
 
-    with open(file_name, newline='') as csvfile:
+    with open(os.path.join('data', file_name), newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             # print(row)
@@ -38,12 +38,12 @@ def tt_split_csv(genre):
             else:
                 train_rows.append(row)
     
-    with open(os.path.join('test', genre + '.csv'), 'w', newline='') as f:
+    with open(os.path.join('data', 'test', genre + '.csv'), 'w', newline='') as f:
         writer = csv.writer(f)
         # writer.writerow(['name', 'artist', 'cover_url', 'album_url', 'album_path'])
         writer.writerows(test_rows)
 
-    with open(os.path.join('train', genre + '.csv'), 'w', newline='') as f:
+    with open(os.path.join('data', 'train', genre + '.csv'), 'w', newline='') as f:
         writer = csv.writer(f)
         # writer.writerow(['name', 'artist', 'cover_url', 'album_url', 'album_path'])
         writer.writerows(train_rows)
