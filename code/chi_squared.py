7151fe1eee43956144849959e905a91974fac703
bhthng098 <77995220+bhthng098@users.noreply.github.com>
2021-07-26 13:39:23 -0700

    Update chi_squared.py

--- code/chi_squared.py
+++ code/chi_squared.py
@@ -1,7 +1,27 @@
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import chi2_contingency
-from colorutils import Color
+from colorutils import Color, rgb_to_web
+
+import webcolors
+
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+def get_colour_name(requested_colour):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+    except ValueError:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
 
 def chisquared_independence_test(df, column_a_name, column_b_name):
     """
@@ -35,10 +55,16 @@ def main():
     train_data['color'] = 0
     
     for i in range(train_data['color'].size):
-        rgb_val = (train_data['dominant-h'][i], train_data['dominant-s'][i], train_data['dominant-v'][i])
-        train_data['color'][i] = Color(rgb=rgb_val).web
+        rgb_val = (int(train_data['dominant-v'][i]), int(train_data['dominant-s'][i]), int(train_data['dominant-h'][i]))
+        _, color_name = get_colour_name(rgb_val)
+        train_data['color'][i] = color_name
+        #rgb_to_web(rgb_val)
+    print(train_data['color'])
 
-    tstats, pval = chisquared_independence_test(train_data, 'color', 'valence')
+    features = ['popularity','explicit','danceability','energy','key','loudness','modality','valence','speechiness']
+    for feature in features:
+        print('\nchi-sq test: color vs', feature)
+        tstats, pval = chisquared_independence_test(train_data, 'color', feature)
 
 if __name__ == "__main__":
     main()
